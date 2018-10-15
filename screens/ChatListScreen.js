cba756ecaeebe3e2102c489971c12ed3576765f8
reubin <reubinder@gmail.com>
2018-10-15 16:55:15 -0400

    added user chat card, mock data

--- screens/ChatListScreen.js
+++ screens/ChatListScreen.js
@@ -1,16 +1,41 @@
 import React, { Component } from "react";
-import { View, Text, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableHighlight,
+  StyleSheet
+} from "react-native";
 import { List, ListItem } from "react-native-elements";
+import { SearchBar } from "react-native-elements";
 
 const users = [
   {
-    name: "Reubin",
+    name: "bob",
     avatar_url:
       "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
     subtitle: "Vice President"
   },
   {
-    name: "Chris Jackson",
+    name: "bob Jackson",
+    avatar_url:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+    subtitle: "Vice Chairman"
+  },
+  {
+    name: "Chris Jackson1",
+    avatar_url:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+    subtitle: "Vice Chairman"
+  },
+  {
+    name: "bob Jackson2",
+    avatar_url:
+      "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
+    subtitle: "Vice Chairman"
+  },
+  {
+    name: "bob Jackson3",
     avatar_url:
       "https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg",
     subtitle: "Vice Chairman"
@@ -18,30 +43,49 @@ const users = [
 ];
 
 export default class ChatListScreen extends Component {
+  constructor() {
+    super();
+  }
+
   static navigationOptions = {
     title: "Chats"
   };
 
-  render() {
+  renderRow({ item }) {
     return (
-      <List>
-        <FlatList
-          data={users}
-          renderItem={this.renderChatCard}
-          keyExtractor={item => item.name}
+      <TouchableHighlight onPress={() => console.log("pressed")}>
+        <ListItem
+          roundAvatar
+          title={item.name}
+          subtitle={item.subtitle}
+          avatar={{ uri: item.avatar_url }}
         />
-      </List>
+      </TouchableHighlight>
     );
   }
 
-  renderChatCard(user) {
+  render() {
     return (
-      <ListItem
-        roundAvatar
-        title={user.name}
-        subtitle={user.subtitle}
-        avatar={user.avatar}
-      />
+      <View>
+        <SearchBar
+          lightTheme
+          icon={{ type: "font-awesome", name: "search" }}
+          placeholder="Type Here..."
+        />
+        <List style={styles.listContainerStyle}>
+          <FlatList
+            data={users}
+            renderItem={this.renderRow}
+            keyExtractor={item => item.name}
+          />
+        </List>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  listContainerStyle: {
+    flex: 1
+  }
+});
